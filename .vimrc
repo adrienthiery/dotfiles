7f5fa995aea1f01729ac0235a09864119db6d7e7
Adrien Thiery <adrien.thiery@gmail.com>
2015-08-15 12:54:54 +0200

    install should work well now

--- .vimrc
+++ .vimrc
@@ -1,3 +1,20 @@
+set nocompatible              " be iMproved, required
+filetype off                  " required
+
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+" let Vundle manage Vundle, required
+Plugin 'VundleVim/Vundle.vim'
+
+Plugin 'kchmck/vim-coffee-script'
+syntax enable
+
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required
+
 set tabstop=2 softtabstop=0 expandtab shiftwidth=2 smarttab
 set colorcolumn=80
 highlight ColorColumn ctermbg=red
--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/zsh
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 BACKUP_DIR=$DIR'/backup-'`date +"%Y%M%d%H%M%S"`'/'
+DEST_DIR=~/
 mkdir -p $BACKUP_DIR
 echo "Backing up actual dotfiles in $BACKUP_DIR"
 
 myDotfiles=(.*)
-myHomeDirDotfiles=${myDotfiles/\./~\/\.}
-
-mv $myHomeDirDotfiles $BACKUP_DIR/
-cp $myDotfiles ~/
+myDotfilesArray=(${myDotfiles// / })
+echo $myDotfilesArray
+for element in "${myDotfilesArray[@]}"
+do
+  if [ "$element" != ".git" ]; then
+    echo "Backing up $element and replacing it"
+    mv ${DEST_DIR}${element} ${BACKUP_DIR}
+    cp -R ${element} ${DEST_DIR}
+  fi
+done
 
 echo "Restart your shell now"
